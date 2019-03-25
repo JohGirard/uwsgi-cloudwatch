69e8e0de79c693698cd3e20ddc9a71ec27040b83
Johannes <johannes.girard@gmail.com>
2019-03-25 11:16:13 +0100

    New Version

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='uwsgi-cloudwatch',
-    version='0.0.1.2',
+    version='0.0.1.4',
     packages=find_packages(exclude=('etc')),
     include_package_data=True,
-    author='Justin Stewart',
-    author_email='jstewart@wdtinc.com',
+    author='Johannes Girard Forked from Justin Stewart',
+    author_email='johannes.girard@gmail.com',
     description='uwsgi-cloudwatch',
-    url='https://github.com/wdtinc/uwsgi-cloudwatch',
+    url='https://github.com/JohGirard/uwsgi-cloudwatch',
     install_requires=[
         'click==6.6',
-        'requests==2.11.1',
-        'boto3==1.4.0',
+        'requests==2.10.0',
+        'boto3==1.9.73',
         'arrow==0.8.0'
     ],
     zip_safe=False,
--- uwsgi_cloudwatch/main.py
+++ uwsgi_cloudwatch/main.py
@@ -5,6 +5,7 @@ import click
 import logging
 import re
 import requests
+import urllib.request
 from . import validation
 
 logging.basicConfig(level=logging.INFO)
@@ -30,11 +31,22 @@ def run_periodically(func, interval):
     _set()
 
 
+def retrieve_asg(region, instance_id):
+    asg = boto3.client('autoscaling', region_name=region)
+    response = asg.describe_auto_scaling_instances(InstanceIds=[instance_id])
+    asg_info = response['AutoScalingInstances']
+    asg_name = asg_info[0]['AutoScalingGroupName']
+    return asg_name
+
+
 def put_metrics(metrics, region, namespace, metric_prefix):
     """ Puts metrics in target CloudWatch namespace. """
 
     timestamp = arrow.get().datetime
     client = boto3.client('cloudwatch', region_name=region)
+    instance_id = urllib.request.urlopen(
+        'http://169.254.169.254/latest/meta-data/instance-id').read().decode()
+    asg_name = retrieve_asg(region, instance_id)
 
     # Assemble Metrics
     metric_data = []
@@ -54,6 +66,24 @@ def put_metrics(metrics, region, namespace, metric_prefix):
             for i in value:
                 metric_data.append({
                     'MetricName': metric_prefix + name,
+                    'Dimensions': [
+                        {
+                            'Name': 'By Instance ID',
+                            'Value': instance_id
+                        },
+                    ],
+                    'Timestamp': timestamp,
+                    'Value': i,
+                    'Unit': unit
+                })
+                metric_data.append({
+                    'MetricName': metric_prefix + name,
+                    'Dimensions': [
+                        {
+                            'Name': 'By Auto Scaling Group',
+                            'Value': asg_name
+                        },
+                    ],
                     'Timestamp': timestamp,
                     'Value': i,
                     'Unit': unit
@@ -61,6 +91,24 @@ def put_metrics(metrics, region, namespace, metric_prefix):
         else:
             metric_data.append({
                 'MetricName': metric_prefix + name,
+                'Dimensions': [
+                    {
+                        'Name': 'By Instance ID',
+                        'Value': instance_id
+                    },
+                ],
+                'Timestamp': timestamp,
+                'Value': value,
+                'Unit': unit
+            })
+            metric_data.append({
+                'MetricName': metric_prefix + name,
+                'Dimensions': [
+                    {
+                        'Name': 'By Auto Scaling Group',
+                        'Value': asg_name
+                    },
+                ],
                 'Timestamp': timestamp,
                 'Value': value,
                 'Unit': unit
@@ -137,8 +185,10 @@ def generate_metrics(stats):
         m['WorkersSignalQueueCount'].append(worker['signal_queue'])
         m['WorkersRssBytes'].append(worker['rss'])
         m['WorkersVszBytes'].append(worker['vsz'])
-        m['WorkersAverageResponseTimeMilliseconds'].append(int(worker['avg_rt']/1000))
-        m['WorkersRunningTimeMilliseconds'].append(int(worker['running_time']/1000))
+        m['WorkersAverageResponseTimeMilliseconds'].append(
+            int(worker['avg_rt']/1000))
+        m['WorkersRunningTimeMilliseconds'].append(
+            int(worker['running_time']/1000))
         m['WorkersLastSpawnTimestamp'].append(worker['last_spawn'])
         m['WorkersRespawnCount'].append(worker['respawn_count'])
 
@@ -194,8 +244,9 @@ def update_cloudwatch_metrics(stats_server, region, namespace, metric_prefix):
 @click.argument('stats-server')
 @click.option('--region', default='us-east-1')
 @click.option('--namespace', required=True, callback=validation.namespace)
-@click.option('--frequency', default=60, callback=validation.frequency)
+@click.option('--frequency', default=30, callback=validation.frequency)
 @click.option('--metric-prefix', default='uWSGI', callback=validation.prefix)
 def cli(stats_server, region, namespace, frequency, metric_prefix):
-    run_periodically(update_cloudwatch_metrics(stats_server, region, namespace, metric_prefix), frequency)
+    run_periodically(update_cloudwatch_metrics(
+        stats_server, region, namespace, metric_prefix), frequency)
     asyncio.get_event_loop().run_forever()
